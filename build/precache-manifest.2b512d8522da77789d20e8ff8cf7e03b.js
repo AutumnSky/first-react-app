cf18d690784ebedd9437940449a31c54aba803b8
MinJeong Kim <capu1004@gmail.com>
2018-12-01 12:01:25 +0900

    add build directory

---
+++ build/precache-manifest.2b512d8522da77789d20e8ff8cf7e03b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6267b87b6dfb2e478987",
+    "url": "/static/js/main.6267b87b.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "6267b87b6dfb2e478987",
+    "url": "/static/css/main.278016cc.chunk.css"
+  },
+  {
+    "revision": "fd32e025a079286261c9a3539e9b0c92",
+    "url": "/index.html"
+  }
+];
